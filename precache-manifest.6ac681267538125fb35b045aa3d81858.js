9211b6c628ec772b210f147bc44040f6d38286ee
zuaam <46837548+zuaam@users.noreply.github.com>
2019-09-28 07:04:49 -0700

    Updates

---
+++ precache-manifest.6ac681267538125fb35b045aa3d81858.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8c2e554c89b2a5e68fdb6e7d4ca967",
+    "url": "/react-redux-app/index.html"
+  },
+  {
+    "revision": "8cfcd0793566c32affd4",
+    "url": "/react-redux-app/static/css/main.96dbca6a.chunk.css"
+  },
+  {
+    "revision": "0ab5f468b96f4a6fcb8c",
+    "url": "/react-redux-app/static/js/2.540975b8.chunk.js"
+  },
+  {
+    "revision": "8cfcd0793566c32affd4",
+    "url": "/react-redux-app/static/js/main.d7cc6ebc.chunk.js"
+  },
+  {
+    "revision": "8e3e29f059b2f7ec77db",
+    "url": "/react-redux-app/static/js/runtime-main.fa769bae.js"
+  }
+]);
